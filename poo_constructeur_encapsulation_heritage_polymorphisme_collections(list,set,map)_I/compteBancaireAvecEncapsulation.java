319f8d756ed7a592a5dd9e2b48766902b75e1ca0
Rgaibi <karimrgaibi@gmail.com>
2025-08-03 19:20:47 +0000

    renommer

---
+++ poo_constructeur_encapsulation_heritage_polymorphisme_collections(list,set,map)_I/compteBancaireAvecEncapsulation.java
@@ -0,0 +1,16 @@
+public class compteBancaireAvecEncapsulation {
+    private double solde;
+    public void deposer(double montant) {
+        if(montant > 0){
+            solde+ = montant;
+        }
+    }
+    public double getsolde(){
+        return solde;
+    }
+    public static void main(String[] args) {
+        compteBancaireAvecEncapsulation c = new compteBancaireAvecEncapsulation();
+        c.deposer(100);
+        System.out.println("solde : "+c.getsolde());
+    }
+}
